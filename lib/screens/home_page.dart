dafbfe7073c1e8f624579b158c00bab4e3055866
Seyit Mustafa Demir <mustafaylmz1184@gmail.com>
2022-05-22 01:00:44 +0300

    cinsiyet-yaş ve çalışma alanının sorulduğu sayfa ve push butonu

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,37 +1,33 @@
 import 'package:antello/classes/app_user.dart';
 import 'package:flutter/material.dart';
-
+import 'package:antello/widgets/push_button.dart';
 import '../widgets/user_chart.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
-
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-         backgroundColor: Colors.black,
-
-        body: SingleChildScrollView(child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-
-            children: [
-             
-
-              UserChart(appuser:  AppUser(ad: "Yusuf", soyad: "ÖZil",bio: "slkflksd"),),
-
-            ],
-          ),
-        )),
-
-    
+      backgroundColor: Colors.black,
+      body: SingleChildScrollView(
+          child: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            UserChart(
+              appuser: AppUser(ad: "Yusuf", soyad: "ÖZil", bio: "slkflksd"),
+            ),
+            PushButton(butonyazisi:"klj",newroute: "/second"),
+          ],
+        ),
+      )),
     );
   }
 }

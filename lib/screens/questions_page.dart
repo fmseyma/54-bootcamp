b1509c2fd46ba35572884913870d9ffbf899f6c6
fmseyma <92673592+fmseyma@users.noreply.github.com>
2022-05-29 00:03:24 +0300

    introduce widget soru sayfasına eklendi

--- lib/screens/questions_page.dart
+++ lib/screens/questions_page.dart
@@ -76,6 +76,7 @@ class _QuestionsPageState extends State<QuestionsPage> {
                   GenderWidget(),
                   BirthdayWidget(),
                   StudyWidget(),
+                  IntroduceWidget(),
                 ],
               ),
             ),
